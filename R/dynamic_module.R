a302f27899661c59aa344f3dab9581bd35400e89
Ashley Baldry <arbaldry91@gmail.com>
2024-06-14 17:48:22 +0100

    Trying to inject the destroying into a created module

--- R/dynamic_module.R
+++ R/dynamic_module.R
@@ -14,46 +14,68 @@ destroyableModuleUI <- function(id, ui, wrapper = div) {
 
 #' @rdname destroyableModule
 #' @export
-destroyableModuleServer <- function(id, module, session = getDefaultReactiveDomain()) {
-  module <- addDestroyers(id, module, session)
-  shiny::moduleServer(id, module, session)
+makeModuleServerDestroyable <- function(module_fn) {
+  stopifnot("`module_fn` is not a function" = is.function(module_fn))
+
+  module_fn_body <- module_fn |> rlang::fn_body() |> as.list()
+  if (isFALSE(any(purrr::map_lgl(module_fn_body, isModuleServerCall)))) {
+    stop("There must be a call to shiny::moduleServer in the `module_fn` function", call. = FALSE)
+  }
+
+  module_fn_w_destroyers <- purrr::modify_if(
+    module_fn_body,
+    isModuleServerCall,
+    addModuleDestroyers
+  )
+
+  rlang::fn_body(module_fn) <- as.call(module_fn_w_destroyers)
+  invisible(module_fn)
 }
 
-addDestroyers <- function(id, module, session = getDefaultReactiveDomain()) {
-  id <- session$ns(id)
+addModuleDestroyers <- function(module) {
+  module_args <- module |> rlang::call_match(fn = moduleServer) |> rlang::call_args()
 
-  module_body <- rlang::fn_body(module)
-  module_body <- as.list(module_body)
-  module_body <- purrr::imap(module_body, assignObserve, id = id)
+  args_w_destroyers <- purrr::modify_if(
+    module_args,
+    "module",
+    addDestroyers
+  )
 
-  module_body <- append(module_body, createCheckDestroyInit(id), 1L)
-
-  rlang::fn_body(module) <- as.call(module_body)
-  invisible(module)
+  module_w_destroyers <- rlang::call2(rlang::call_name(module), !!!args_w_destroyers)
+  invisible(module_w_destroyers)
 }
 
-assignObserve <- function(fn_call, idx, id) {
+addDestroyers <- function(module) {
+  module_body <- module |> rlang::() |> as.list()
+
+}
+
+assignObserve <- function(fn_call, idx) {
   if (isObserver(fn_call)) {
-    assign_val <- str2lang(paste0("session$userData$.shiny.destroy[[\"", id, "\"]][[\"obs_", idx, "\"]]"))
+    assign_val <- str2lang(paste0("session$userData$.shiny.destroy[[session$ns(NULL)]][[\"obs_", idx, "\"]]"))
     rlang::call2("<-", assign_val, fn_call)
   } else {
     fn_call
   }
 }
 
-isObserver <- function(fn_call) {
+isSpecifiedFunction <- function(fn_call, fns) {
   if (!is.language(fn_call) || is.symbol(fn_call)) return(FALSE)
 
-  rlang::call_name(fn_call) %in% OBSERVE_FNS ||
-    (rlang::call_name(fn_call) %in% ASSIGN_FNS && rlang::call_name(fn_call[[3L]]) %in% OBSERVE_FNS)
+  fn_name <- rlang::call_name(fn_call)
+  fn_name %in% fns || (fn_name %in% ASSIGN_FNS && rlang::call_name(fn_call[[3L]]) %in% fns)
 }
 
+isModuleServerCall <- function(fn_call) isSpecifiedFunction(fn_call, MODULE_FNS)
+isObserver <- function(fn_call) isSpecifiedFunction(fn_call, OBSERVE_FNS)
+
+MODULE_FNS <- "moduleServer"
 OBSERVE_FNS <- c("observe", "observeEvent")
 ASSIGN_FNS <- c("<-", "<<-")
 
-createCheckDestroyInit <- function(id) {
+createCheckDestroyInit <- function() {
   list(
     str2lang("if (!\".shiny.destroy\" %in% names(session$userData)) { session$userData$.shiny.destroy <- list() }"),
-    str2lang(paste0("session$userData$.shiny.destroy[[\"", id, "\"]] <- list()"))
+    str2lang(paste0("session$userData$.shiny.destroy[[session$ns(NULL)]] <- list()"))
   )
 }
