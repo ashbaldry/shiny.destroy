2b2b363afcc4495fb0d8130b9db6a6760a8cbab5
Ashley Baldry <ashley.baldry@ascent.io>
2024-07-16 19:33:52 +0100

    Update final tests for example

--- tests/testthat/test-example_app.R
+++ tests/testthat/test-example_app.R
@@ -27,6 +27,14 @@ test_that("01_boxes application works as expected", {
   app$click(input = "card_1-destroy")
   expect_equal(app$get_value(input = "create"), 2L, ignore_attr = TRUE)
   expect_identical(app$get_value(output = "n_modules"), "1")
-  expect_equal(app$get_value(input = "card_1-destroy"), 0L, ignore_attr = TRUE)
+  expect_null(app$get_value(input = "card_1-destroy"))
   expect_match(app$get_value(output = "inputs"), "create: 2\ncard_2")
+
+  app$click(input = "create")
+  expect_equal(app$get_value(input = "create"), 3L, ignore_attr = TRUE)
+  expect_identical(app$get_value(output = "n_modules"), "2")
+  expect_match(
+    app$get_value(output = "inputs"),
+    "create: 2.*card_2-destroy: 0.*card_3-destroy: 0"
+  )
 })
