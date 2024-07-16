54089c165e27077f6e5c2d7eecafed0f97d5ffdf
Ashley Baldry <ashley.baldry@ascent.io>
2024-07-16 19:31:26 +0100

    Add tests related to example application

---
+++ tests/testthat/test-example_app.R
@@ -0,0 +1,32 @@
+test_that("01_boxes application works as expected", {
+  example_dir <- system.file("examples-shiny", "01_boxes", package = "shiny.destroy")
+  app <- shinytest2::AppDriver$new(example_dir, name = "example_app")
+  on.exit(app$stop())
+
+  expect_equal(app$get_value(input = "create"), 0L, ignore_attr = TRUE)
+  expect_identical(app$get_value(output = "n_modules"), "0")
+
+  app$click(input = "create")
+  expect_equal(app$get_value(input = "create"), 1L, ignore_attr = TRUE)
+  expect_identical(app$get_value(output = "n_modules"), "1")
+  expect_equal(app$get_value(input = "card_1-destroy"), 0L, ignore_attr = TRUE)
+  expect_match(
+    app$get_value(output = "inputs"),
+    "create: 1.*card_1-destroy: 0"
+  )
+
+  app$click(input = "create")
+  expect_equal(app$get_value(input = "create"), 2L, ignore_attr = TRUE)
+  expect_identical(app$get_value(output = "n_modules"), "2")
+  expect_equal(app$get_value(input = "card_1-destroy"), 0L, ignore_attr = TRUE)
+  expect_match(
+    app$get_value(output = "inputs"),
+    "create: 2.*card_1-destroy: 0.*card_2-destroy: 0"
+  )
+
+  app$click(input = "card_1-destroy")
+  expect_equal(app$get_value(input = "create"), 2L, ignore_attr = TRUE)
+  expect_identical(app$get_value(output = "n_modules"), "1")
+  expect_equal(app$get_value(input = "card_1-destroy"), 0L, ignore_attr = TRUE)
+  expect_match(app$get_value(output = "inputs"), "create: 2\ncard_2")
+})
