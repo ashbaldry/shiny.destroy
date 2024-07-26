978441da5fc500bf64d6a9721084cceffc82ac47
Ashley Baldry <ashley.baldry@ascent.io>
2024-07-26 19:35:47 +0100

    Fix markdown example

--- R/removeInput.R
+++ R/removeInput.R
@@ -3,10 +3,19 @@
 #' The removal of the named input in a shiny session.
 #'
 #' @param id Input value name
+#' @param selector The HTML selector to remove the UI for. By default it is the
+#' tag where the ID matches the input, but might need to be adjusted for
+#' different inputs.
 #' @param session The Shiny session to remove the input from
 #'
 #' @return
 #' An invisible `TRUE` value confirming that the input has been removed.
+#'
+#' @details
+#' If the input is a standard `{shiny}` input e.g. `numericInput`, then to
+#' remove the label as well as the input, set the selector to be
+#' `paste0(":has(> #", id, ")")`
+#'
 #'
 #' @examplesIf interactive()
 #' library(shiny)
@@ -19,16 +28,19 @@
 #' )
 #'
 #' server <- function(input, output, session) {
-#'   output$number_out <- renderText(input$number)
+#'   output$number_out <- renderText(input$number %||% "input unavailable")
 #'
-#'   observeEvent(input$delete, removeInput("number"))
+#'   observeEvent(
+#'     input$delete,
+#'     removeInput("number", selector = ":has(> #number)")
+#'    )
 #' }
 #'
 #' shinyApp(ui, server)
 #'
 #' @export
-removeInput <- function(id, session = getDefaultReactiveDomain()) {
-  shiny::removeUI(paste0("#", id), immediate = TRUE, session = session)
+removeInput <- function(id, selector = paste0("#", id), session = getDefaultReactiveDomain()) {
+  shiny::removeUI(selector, immediate = TRUE, session = session)
 
   destroyInput(id, session = session)
   invalidateInputs(session)
