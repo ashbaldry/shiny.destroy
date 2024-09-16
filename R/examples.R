448d6eebf53b8558757d5e092658c2e26c243807
Ashley Baldry <arbaldry91@gmail.com>
2024-09-16 19:07:50 +0100

    Add more output docs

--- R/examples.R
+++ R/examples.R
@@ -7,6 +7,9 @@
 #' @inheritParams shiny::runExample
 #' @param ... Additional parameters sent to `shiny::runExample`
 #'
+#' @return
+#' The shiny application displayed in the specified location.
+#'
 #' @details
 #' The following examples are available:
 #'
@@ -15,6 +18,10 @@
 #' load a simple box with a "destroy" button. This highlights the full
 #' removal of the module when the button is pressed.}
 #' }
+#' \item{02_sleep}{An application that has 2 side by side modules, one
+#' using \{shiny\} to remove the UI and the other using \{shiny.destroy\}
+#' to fully remove the boxes to display the incremental time gain from
+#' removing the long-running observers.}
 #'
 #' @examplesIf interactive()
 #' runDestroyExample("01_boxes")
